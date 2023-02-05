213350078f0c6694fa81039ef419ac4462315a6f
Alexey Martynov <martynov.a@selectel.ru>
2023-02-05 11:56:42 +0300

    remove ENVs

--- src/run.sh
+++ src/run.sh
@@ -1,11 +1,8 @@
 docker kill $(docker ps -q)
-export NETWORK=superreddit
-export NETALIAS_MONGO_DB=mongo_db
-export NETALIAS_POST=post
-export NETALIAS_COMMENT=comment
+export NETWORK=reddit
 
 docker network create "${NETWORK}"
-docker run -d --network-alias=post_db --network-alias=comment_db mongo:latest
-docker run -d --network-alias=post catalogoanatom/post:1.0
-docker run -d --network-alias=comment catalogoanatom/comment:1.0
-docker run -d  -p 9292:9292 catalogoanatom/ui:1.0
+docker run -d --network=${NETWORK} --network-alias=post_db --network-alias=comment_db -v reddit_db:/data/db mongo:latest
+docker run -d --network=${NETWORK} --network-alias=post catalogoanatom/post:1.0
+docker run -d --network=${NETWORK} --network-alias=comment catalogoanatom/comment:1.0
+docker run -d --network=${NETWORK}  -p 9292:9292 catalogoanatom/ui:2.0
